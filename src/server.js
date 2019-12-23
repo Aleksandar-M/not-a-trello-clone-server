949a3f35112ad93a03d7b33fe29dd72072b9e475
Alex <m.aleksandar1994@gmail.com>
2019-12-23 22:18:01 +0100

    Created and connected to new database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: `${__dirname}/../.env` });
 const app = require('./app');
+
+const url = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(url, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+}).then(() => { console.log('Successfully connected to database'); });
+
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
