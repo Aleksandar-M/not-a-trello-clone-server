b41c38a731848a838a1fb39370ec3a7958289fa1
Alex <m.aleksandar1994@gmail.com>
2019-12-27 00:14:32 +0100

    CRUD operations moved to new controller

---
+++ src/controllers/handleCRUD.js
@@ -0,0 +1,94 @@
+
+exports.getAll = (Model) => async (req, res) => {
+	try {
+		const result = await Model.find({}).populate('tab', 'author');
+
+		res.status(200).json({
+			message: 'Success',
+			data: {
+				result,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.getOne = (Model, populateOptions) => async (req, res) => {
+	try {
+		let query = Model.findById(req.params.id);
+		if (populateOptions) query = query.populate(populateOptions);
+
+		const result = await query;
+
+		if (!result) {
+			console.log('Card not found in DB');
+		}
+
+		res.status(200).json({
+			message: 'Success',
+			data: {
+				result,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.createOne = (Model) => async (req, res) => {
+	// TODO: append user and tab id's
+	try {
+		const result = await Model.create(req.body);
+
+		res.status(201).json({
+			message: 'Success',
+			data: {
+				result,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.updateOne = (Model) => async (req, res) => {
+	try {
+		const result = await Model.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!result) {
+			console.log('Card not found in DB');
+		}
+
+		res.status(201).json({
+			message: 'Success',
+			data: {
+				result,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+exports.deleteOne = (Model) => async (req, res) => {
+	try {
+		const result = await Model.findByIdAndDelete(req.params.id);
+
+		if (!result) {
+			console.log('Card not found in DB');
+		}
+
+		res.status(204).json({
+			status: 'Success',
+			data: {
+				result,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
